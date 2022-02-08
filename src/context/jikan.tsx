92594c9c0c5ceb70efd49a1bd3f9b39898d2a045
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-02-08 18:11:47 -0300

    🏗️ build: context jikan useEffect

--- src/context/jikan.tsx
+++ src/context/jikan.tsx
@@ -86,6 +86,7 @@ export function JikanContextProvider({ children }: jikanContextProps) {
 
   useEffect(() => {
     jikanBase();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
