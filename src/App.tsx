9b7c1b26725462e3f4f0f62e1c329039d2631143
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-03-05 03:19:33 -0300

    🐛 fix: body in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
-import { Busca } from './components/busca';
-import { FormAdd } from './components/formAdd';
-import { Navegation } from './components/navegation';
-import { TabsButton } from './components/tabs';
-import { AppStyle } from './styles/style';
+import { Busca } from "./components/busca";
+import { FormAdd } from "./components/formAdd";
+import { Navegation } from "./components/navegation";
+import { TabsButton } from "./components/tabs";
+import { AppStyle } from "./styles/style";
 
 export function App() {
   const path = "/" + process.env.REACT_APP_MY_ENVIRONMENT_VARIABLE;
 
   return (
     <>
-      <body className="App-header">
+      <div className="App-header">
         <Switch>
           <Route path="/" exact component={TabsButton} />
           <Route path={path} exact component={FormAdd} />
@@ -20,7 +20,7 @@ export function App() {
         </Switch>
 
         <AppStyle />
-      </body>
+      </div>
       <Navegation />
     </>
   );
