fe09447aa3c81abcdd0e1d349abd04a47d74558c
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-04-14 16:20:47 -0300

    ✨ feat: data package created

---
+++ src/data/data.ts
@@ -0,0 +1,77 @@
+import { inputObj } from "../model/animes";
+
+export const dayObject: inputObj[] = [
+  {
+    value: "segunda",
+    number: 1,
+  },
+  {
+    value: "terca",
+    number: 2,
+  },
+  {
+    value: "quarta",
+    number: 3,
+  },
+  {
+    value: "quinta",
+    number: 4,
+  },
+  {
+    value: "sexta",
+    number: 5,
+  },
+  {
+    value: "sabado",
+    number: 6,
+  },
+  {
+    value: "domingo",
+    number: 7,
+  },
+];
+
+export const launchStated: inputObj[] = [
+  {
+    value: "true",
+    number: 1,
+  },
+  {
+    value: "false",
+    number: 0,
+  },
+];
+
+export const season: inputObj[] = [
+  {
+    value: "winter",
+    number: 0,
+  },
+  {
+    value: "spring",
+    number: 1,
+  },
+  {
+    value: "summer",
+    number: 2,
+  },
+  {
+    value: "fall",
+    number: 3,
+  },
+];
+
+export const year: inputObj[] = [
+  {
+    value: "2022",
+    number: 0,
+  },
+  {
+    value: "2023",
+    number: 1,
+  },
+  {
+    value: "2024",
+    number: 2,
+  },
+];
